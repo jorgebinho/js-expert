610ce889cfe09520d0311038855f9e45b240e425
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2024-05-18 19:27:56 -0300

    ✅ Add teste de retorno caso sucesso

--- testing/index.test.js
+++ testing/index.test.js
@@ -25,4 +25,29 @@ const assert = require('assert')
         await assert.rejects(result, expected)
     }
 
+    {
+        const filePath = './mock/threeItems-valid.csv'
+        const expected = [
+            {
+                id: 1,
+                name: "durval",
+                profession: "developer",
+                age: 34
+            },
+            {
+                id: 2,
+                name: "elano",
+                profession: "manager",
+                age: 30
+            },
+            {
+                id: 3,
+                name: "jose",
+                profession: "QA",
+                age: 25
+            }
+        ]
+        const result = await File.csvToJSON(filePath)
+        assert.deepEqual(result, expected)
+    }
 })()
--- testing/src/file.js
+++ testing/src/file.js
@@ -12,6 +12,8 @@ class File {
         const content = await readFile(filePath, "utf8")
         const validation = this.isValid(content)
         if (!validation.valid) throw new Error(validation.error)
+
+        return this.parseCSVToJSON(content)
     }
 
     static isValid(csvString, options = DEFAULT_OPTIONS) {
@@ -32,6 +34,25 @@ class File {
             valid: false
         }
        }
+
+       return { valid: true}
+    }
+
+    static parseCSVToJSON (csvString) {
+        const lines = csvString.split(/\r?\n/)
+
+        const firstLine = lines.shift()
+        const header = firstLine.split(',')
+
+        const users = lines.map(line => {
+            const columns = line.split(',')
+            const user = {}
+            for (const index in columns) {
+                user[header[index]] = columns[index].trim()
+            }
+            return user
+        })
+        return users
     }
 }
 
